e9025913b1e961fd9983b2541015af582b317645
272727ana <96492887+272727ana@users.noreply.github.com>
2021-12-28 23:47:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let phoneNumber;
+phoneNumber = 30;
+
+let priceRate;
+priceRate = 5;
+
+let phoneRice;
+phoneRice = 119.95
+
+let priceRateDecimal;
+priceRateDecimal = priceRate / 100;
+
+
+let fullPrice;
+fullPrice = phoneNumber * phoneRice * priceRateDecimal;
+console.log(fullPrice);
+
+let pi = 3.14;
+let area;
+let r;
+r = prompt("koj e radiusot na krugot")
+area = pi * r ** 2;
+console.log(area)
+
